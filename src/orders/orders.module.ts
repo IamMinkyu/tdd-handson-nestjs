c0aa8254661e4127d2991e66b9b78b77aca9bfc4
mklee-mac <mklee@headit.co.kr>
2023-06-20 22:59:49 +0900

    first commit

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,14 +3,12 @@ import { OrdersController } from './controllers/orders.controller';
 import { OrdersService } from './orders.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Orders } from './entity/orders.entity';
-import { StartOrderService } from './commands/start-order/start-order.service';
-import { PlaceOrderService } from './commands/place-order/place-order.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Orders]),
     ],
     controllers: [OrdersController],
-    providers: [OrdersService, StartOrderService, PlaceOrderService]
+    providers: [OrdersService]
 })
 export class OrdersModule { }
